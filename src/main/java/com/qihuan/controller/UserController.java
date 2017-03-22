f2af50ed55cbdb2521abfdfac49096a740d162cf
qihuan <qihuan@ejiaoji.com>
2017-03-22 14:34:29 +0800

    集成swagger，提供在线文档支持

--- src/main/java/com/qihuan/controller/UserController.java
+++ src/main/java/com/qihuan/controller/UserController.java
@@ -4,16 +4,16 @@ import com.qihuan.pojo.User;
 import com.qihuan.service.UserService;
 import com.qihuan.tools.Const;
 import com.qihuan.tools.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * UserController
  * Created by Qi on 2017/3/14.
  */
+@Api(value="用户管理",tags = {"用户管理API"},description = "用户管理接口")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -21,7 +21,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/register")
+    @ApiOperation(value = "注册用户")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
     public Result register(@RequestParam String userName,
                            @RequestParam String password,
                            @RequestParam String nickName) {
@@ -33,14 +34,16 @@ public class UserController {
         return Result.create(0, Const.SUCCESS, user);
     }
 
-    @RequestMapping("/userList")
+    @ApiOperation(value = "获取用户列表")
+    @RequestMapping(value = "/userList", method = RequestMethod.GET)
     @ResponseBody
     public Result userList(@RequestParam(defaultValue = "0") int page,
                            @RequestParam(defaultValue = "10") int size) {
         return Result.create(0, Const.SUCCESS, userService.userList(page, size));
     }
 
-    @RequestMapping("/getUser")
+    @ApiOperation(value = "根据用户名获取用户")
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET)
     public Result userList(@RequestParam String userName) {
         return Result.create(0, Const.SUCCESS, userService.getUser(userName));
     }
